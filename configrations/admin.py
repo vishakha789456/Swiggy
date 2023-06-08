e924d67926c2e81c103488e73a55619ce0cf9cf2
Vishakha Arya <vishakhaarya0508@gmail.com>
2023-06-08 13:06:24 +0530

    Swiggyconfigurations

---
+++ configrations/admin.py
@@ -0,0 +1,3 @@
+from django.contrib import admin
+from configrations.models import*
+admin.site.register(MyUser)
---
+++ configrations/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ConfigrationsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'configrations'
